987030085c7fb87410e488d608bf4e0d21a427a0
Patrick Fulghum <patrick.c.fulghum@gmail.com>
2018-02-08 17:01:56 -0500

    Write Body of Node and Link prototype functions

--- js/DialogTree.js
+++ js/DialogTree.js
@@ -1,13 +1,8 @@
-export const Node = function(name, primaryKey, line) {
+export const Node = function(name, primaryKey, line, links) {
   this.name = name;
   this.primaryKey = primaryKey;
   this.line = line;
-};
-
-export const Link = function(lines, scripts, primaryKey, previousNode) {
-  this.lines = lines;
-  this.scripts = scripts;
-  this.primaryKey = primaryKey;
+  this.links = links;
 };
 
 Node.prototype.getId = function() {
@@ -19,35 +14,37 @@ Node.prototype.setId = function(inputKey) {
 };
 
 Node.prototype.getLinks = function() {
-
+  return this.links;
 };
 
-Node.prototype.setLink = function(linkObject) {
-
+Node.prototype.addLink = function(linkObject) {
+  this.links.push(linkObject);
 };
 
 Node.prototype.getLink = function(n) {
+  return this.links[n];
+};
 
+export const Link = function(lines, primaryKey, nextNode) {
+  this.lines = lines;
+  this.primaryKey = primaryKey;
+  this.nextNode = nextNode;
 };
 
 Link.prototype.getLines = function() {
-
+  return this.lines;
 };
 
 Link.prototype.getLine = function(n) {
-
+  return this.lines[n];
 };
 
 Link.prototype.getNextNode = function() {
-
+  return this.nextNode;
 };
 
-Link.prototype.setNextNode = function() {
-
-};
-
-Link.prototype.getPreviousNode = function() {
-
+Link.prototype.setNextNode = function(n) {
+  this.nextNode = n;
 };
 
 
